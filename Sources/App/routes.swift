39263c69ee9c8e77ba80faba5d3e9eccff076c66
Cory Steers <developer@madsteer.com>
2018-12-30 13:03:33 -0600

    started working on a web front-end

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,44 +6,52 @@ import Foundation
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    router.get { req -> Future<View> in
-        let context = [String: String]()
+    router.get("polls") { req -> Future<View> in
+        let context = [String: [Poll]]()
+        let allPolls = Poll.query(on: req).all().map(to: [Poll])
+        let allPollsDict =
+        context.append("allPools", allPolls)
         return try req.view().render("home", context)
     }
+//    router.get("polls") { req  in
+//        return "Hello, world!"
+//    }
 
-    router.post(Poll.self, at: "polls") { req, poll -> Future<Poll> in
-        return poll.save(on: req)
-    }
-
-    router.put("polls", UUID.parameter, Int.parameter) { req -> Future<Poll> in
-        let id = try req.parameters.next(UUID.self)
-        let vote = try req.parameters.next(Int.self)
-
-        return try Poll.find(id, on: req).flatMap(to: Poll.self) { poll in
-            guard var poll = poll else {
-                throw Abort(.notFound)
-            }
-
-            if vote == 1 {
-                poll.votes1 += 1
-            } else {
-                poll.votes2 += 1
-            }
-
+    router.group("polls", "api") { api in
+        api.post(Poll.self, at: "polls") { req, poll -> Future<Poll> in
             return poll.save(on: req)
         }
-    }
 
-    router.get("polls") { req -> Future<[Poll]> in
-        return Poll.query(on: req).all()
-    }
+        api.put("polls", UUID.parameter, Int.parameter) { req -> Future<Poll> in
+            let id = try req.parameters.next(UUID.self)
+            let vote = try req.parameters.next(Int.self)
 
-    router.get("poll", UUID.parameter) { req -> Future<Poll> in
-        return Poll.find(try req.parameters.next(UUID.self), on: req).unwrap(or: Abort(.notFound))
-    }
+            return Poll.find(id, on: req).flatMap(to: Poll.self) { poll in
+                guard var poll = poll else {
+                    throw Abort(.notFound)
+                }
 
-    router.delete("poll", UUID.parameter) { req -> Future<HTTPStatus> in
-        return Poll.find(try req.parameters.next(UUID.self), on: req).unwrap(or: Abort(.notFound)).delete(on: req).transform(to: .noContent)
+                if vote == 1 {
+                    poll.votes1 += 1
+                } else {
+                    poll.votes2 += 1
+                }
 
+                return poll.save(on: req)
+            }
+        }
+
+        api.get("polls") { req -> Future<[Poll]> in
+            return Poll.query(on: req).all()
+        }
+
+        api.get("poll", UUID.parameter) { req -> Future<Poll> in
+            return Poll.find(try req.parameters.next(UUID.self), on: req).unwrap(or: Abort(.notFound))
+        }
+
+        api.delete("poll", UUID.parameter) { req -> Future<HTTPStatus> in
+            return Poll.find(try req.parameters.next(UUID.self), on: req).unwrap(or: Abort(.notFound)).delete(on: req).transform(to: .noContent)
+
+        }
     }
 }
